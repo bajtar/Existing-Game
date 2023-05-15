2f161af2ac833da9b3e42a3d61dd77192e48e837
bajtar <125398190+bajtar@users.noreply.github.com>
2023-05-15 21:35:17 +0200

    added bubble spawn script

--- Assets/SpawnScript.cs
+++ Assets/SpawnScript.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class SpawnScript : MonoBehaviour
 {
+    public Transform trans;
+    public GameObject bubble;
+    [SerializeField] public float wait = 1f;
+    public float timer = 0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,14 @@ public class SpawnScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        timer += Time.deltaTime;    
         
+        if (timer >= wait)
+        {
+            Instantiate(bubble, transform.position, Quaternion.identity);
+            timer = 0;
+        }
+ 
+       
     }
 }
